ac33df47ec73726f202bb85f61d6b4518d52ee7d
Jaylee89 <121769651@qq.com>
2023-01-25 22:52:08 +0800

    add code for add block plus

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # edge-extension
 Edge extension
+
+## Edge configuration
+
+1. 开发者模式以及允许来自其他应用商店的扩展
+  - edge://extensions/
+
+## doc reference
+
+https://blog.csdn.net/dasu7138/article/details/101316322
+http://www.36nu.com/post/219.html
+http://www.cnplugins.com/down/down.php?fn=1902/ublock-plus-adblocker-1.5.2.crx%20&aid=225799&typeid=19
+https://www.cnblogs.com/liuxianan/p/chrome-plugin-develop.html
+
+## extension
+http://www.cnplugins.com/down/down.php
+sms code: 127650
+http://www.cnplugins.com/zhuanti/pac.html
+
+## 官方资料
+
+推荐查看官方文档，虽然是英文，但是全且新，国内的中文资料都比较旧（注意以下全部需要翻墙）：
+
+* [Chrome插件官方文档主页](https://developer.chrome.com/extensions)
+* [Chrome插件官方示例](https://developer.chrome.com/extensions/samples)
+* [manifest清单文件](https://developer.chrome.com/extensions/manifest)
+* [permissions权限](https://developer.chrome.com/extensions/permissions)
+* [chrome.xxx.api文档](https://developer.chrome.com/extensions/api_index)
+* [模糊匹配规则语法详解](https://developer.chrome.com/extensions/match_patterns)
+
+## 第三方资料
+
+部分中文资料，不是特别推荐：
+
+* [360安全浏览器开发文档](http://open.se.360.cn/open/extension_dev/overview.html)
+* [360极速浏览器Chrome扩展开发文档](http://open.chrome.360.cn/extension_dev/overview.html)
+* [Chrome扩展开发极客系列博客](http://www.cnblogs.com/champagne/p/)
+
+## Good samples
+
+https://github.com/Ninnka/chrome-extension-xpathcrawler
+https://github.com/sxei/chrome-plugin-demo
---
+++ adblockplus/content-script.js
@@ -0,0 +1,54 @@
+﻿(function() {
+	var __x = {}
+	__x.xpath = function(path) {
+		return document.evaluate (
+			path,
+			document,
+			null,
+			XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+			null
+		);
+	}
+
+	ad_ids = ["fd_foot", "lovexin122", "lovexin102", "swtleft", "swtleft", "bottomAD", "bottomAD", "userText"];
+	ad_classes = ["suspensionBg", "suspension"];
+
+	function removeAdByIdOrClassAttributes(array, type) {
+		array.forEach(function(e){
+			var id = $(type + e)
+			if(!!id) {
+				id.remove()
+			} else {
+				console.log(e + " is not exist!")
+			}
+		});
+	}
+
+	ad_img_xpath = ["//img[contains(@src,'.gif')]"]
+
+	function removeAdImages(array) {
+		array.forEach(function(element){
+			var result = __x.xpath(element);
+			if(result && result.length == 1) {
+				result[0].remove();
+			} else if(result && result.length > 1) {
+				result.forEach(function(element2){
+					if(element2.getAttribute("src").endsWith(".gif")) {
+						element2.parentNode.remove();
+					}
+				});
+			}
+		});
+	}
+
+	var internalClock = setInterval(function(){
+		removeAdByIdOrClassAttributes(ad_ids, "#")
+		removeAdByIdOrClassAttributes(ad_classes, ".")
+		removeAdImages(ad_img_xpath)
+		console.log("internalClock function is completed");
+	}, 1000);
+	setTimeout(function(){
+		internalClock = undefined
+		console.log("internalClock function internal is end");
+	}, 3000);
+})();
